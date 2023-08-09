803f7e4c78a1281c2788871acb3237b54bd028d2
Daehan Choi <sheldhe93@gmail.com>
2023-08-09 14:29:33 +0900

    Modify - Skill Page & Delete Skill Page Img

--- src/components/main/contents/index.js
+++ src/components/main/contents/index.js
@@ -3,7 +3,7 @@ import { LinkIcon } from "@heroicons/react/24/outline";
 import Block from "components/block";
 import { Link } from "react-scroll";
 
-//Todo:리팩토링 필요 
+//Todo:리팩토링 필요
 const pageChecker = {
   // main: "aboutme",
   "About Me": "aboutme",
@@ -14,22 +14,22 @@ const pageChecker = {
 };
 
 const Contents = ({ title, bgColor = "bg-white", children }) => {
-  const toUrl =pageChecker[title];
+  const toUrl = pageChecker[title];
 
   return (
     <div
-      className={`w-full flex flex-col justify-center ${bgColor} pt-20 items-center md:pr-0 md:pl-0 pl-2 pr-2` }
+      className={`w-full flex flex-col justify-center items-center ${bgColor} pt-10 pl-2 pr-2 md:pr-0 md:pl-0 md:pt-20`}
     >
       <div className="w-full max-w-screen-lg flex justify-center items-center">
         <div className="w-full flex items-center flex-col">
           <div className="flex justify-center items-start flex-row">
             <div className="h-full pt-3 pr-3 md:block hidden">
               <Link to={toUrl} spy={true} smooth={true}>
-              <LinkIcon
-                className="h-10 w-10 stroke-2  text-neutral-600 cursor-pointer hover:text-purple-500"
-                aria-hidden="true"
-              />
-            </Link>
+                <LinkIcon
+                  className="h-10 w-10 stroke-2  text-neutral-600 cursor-pointer hover:text-purple-500"
+                  aria-hidden="true"
+                />
+              </Link>
             </div>
             <div className=" md:text-5xl text-3xl font-black text-center ">
               <div>{title}</div>
--- src/components/main/skills/index.js
+++ src/components/main/skills/index.js
@@ -3,70 +3,169 @@ import React from "react";
 const Skills = () => {
   return (
     <>
-      <div className="pt-12 flex justify-center items-start md:flex-row flex-col">
-        <div className="md:w-4/12 w-full flex justify-center pr-6 md:pl-0 pl-6 mb-8">
+      <div className="pt-12 flex w-full justify-start items-start md:flex-row flex-col flex-wrap">
+        <div className="md:max-xl:w-6/12 lg:w-4/12 w-full flex justify-center pl-6 pr-6 mb-8">
           <div className="bg-white w-full rounded-xl p-8">
-            <div className="text-2xl font-semibold pb-2 border-b border-grey-500">
+            <div className="text-2xl font-semibold pb-2 border-b border-blue-600">
               Front End
             </div>
             <div className="mt-8">
-              <img src="/img/front_01.png" alt="frontend_01" />
-              <img src="/img/front_02.png" alt="frontend_02" />
-              <img src="/img/front_04.png" alt="frontend_04" />
+              <div className="text-center text-2xl pt-2 pb-2 text-neutral-700 flex items-center justify-start">
+                <div className="rounded-full bg-blue-500 w-2 h-2 mr-4"></div>
+                <div>HTML</div>
+              </div>
+              <div className="text-center text-2xl pt-2 pb-2 text-neutral-700 flex items-center justify-start">
+                <div className="rounded-full bg-blue-500 w-2 h-2 mr-4"></div>
+                <div>CSS</div>
+              </div>
+              <div className="text-center text-2xl pt-2 pb-2 text-neutral-700 flex items-center justify-start">
+                <div className="rounded-full bg-blue-500 w-2 h-2 mr-4"></div>
+                <div>Javascript</div>
+              </div>
+              <div className="text-center text-2xl pt-2 pb-2 text-neutral-700 flex items-center justify-start">
+                <div className="rounded-full bg-blue-500 w-2 h-2 mr-4"></div>
+                <div>SASS</div>
+              </div>
+              <div className="text-center text-2xl pt-2 pb-2 text-neutral-700 flex items-center justify-start">
+                <div className="rounded-full bg-blue-500 w-2 h-2 mr-4"></div>
+                <div>React</div>
+              </div>
+              <div className="text-center text-2xl pt-2 pb-2 text-neutral-700 flex items-center justify-start">
+                <div className="rounded-full bg-blue-500 w-2 h-2 mr-4"></div>
+                <div>Redux</div>
+              </div>
+              <div className="text-center text-2xl pt-2 pb-2 text-neutral-700 flex items-center justify-start">
+                <div className="rounded-full bg-blue-500 w-2 h-2 mr-4"></div>
+                <div>React Saga</div>
+              </div>
+              <div className="text-center text-2xl pt-2 pb-2 text-neutral-700 flex items-center justify-start">
+                <div className="rounded-full bg-blue-500 w-2 h-2 mr-4"></div>
+                <div>React query</div>
+              </div>
+              <div className="text-center text-2xl pt-2 pb-2 text-neutral-700 flex items-center justify-start">
+                <div className="rounded-full  bg-blue-500 w-2 h-2 mr-4"></div>
+                <div>JQuery</div>
+              </div>
+              <div className="text-center text-2xl pt-2 pb-2 text-neutral-700 flex items-center justify-start">
+                <div className="rounded-full  bg-blue-500 w-2 h-2 mr-4"></div>
+                <div>NextJS</div>
+              </div>
+              <div className="text-center text-2xl pt-2 pb-2 text-neutral-700 flex items-center justify-start">
+                <div className="rounded-full  bg-blue-500 w-2 h-2 mr-4"></div>
+                <div>BootStrap</div>
+              </div>
+              <div className="text-center text-2xl pt-2 pb-2 text-neutral-700 flex items-center justify-start">
+                <div className="rounded-full  bg-blue-500 w-2 h-2 mr-4"></div>
+                <div>Tailwindcss</div>
+              </div>
+              <div className="text-center text-2xl pt-2 pb-2 text-neutral-700 flex items-center justify-start">
+                <div className="rounded-full  bg-blue-500 w-2 h-2 mr-4"></div>
+                <div>MUI</div>
+              </div>
             </div>
           </div>
         </div>
-        <div className="md:w-4/12 w-full flex flex-col items-center pr-6 pl-6">
+        <div className="md:max-xl:w-6/12 lg:w-4/12 w-full flex flex-col items-center pr-6 pl-6">
           <div className="bg-white w-full rounded-xl p-8">
-            <div className="text-2xl font-semibold pb-2 border-b border-grey-500">
+            <div className="text-2xl font-semibold pb-2 border-b border-blue-600">
               Back End
             </div>
             <div className="mt-8">
-              <img src="/img/backend_01.png" alt="backend_01" />
-              <div className="text-center text-4xl pt-2 pb-2 text-neutral-500">
-                PHP
+              <div className="text-center text-2xl pt-2 pb-2 text-neutral-700 flex items-center justify-start">
+                <div className="rounded-full bg-blue-500 w-2 h-2 mr-4"></div>
+                <div>Firebase</div>
               </div>
-              <div className="text-center text-4xl pt-2 pb-2 text-neutral-500">
-                C#
+              <div className="text-center text-2xl pt-2 pb-2 text-neutral-700 flex items-center justify-start">
+                <div className="rounded-full bg-blue-500 w-2 h-2 mr-4"></div>
+                <div>PHP</div>
               </div>
-              <div className="text-center text-4xl pt-2 pb-2 text-neutral-500">
-                JAVA
+              <div className="text-center text-2xl pt-2 pb-2 text-neutral-700 flex items-center justify-start">
+                <div className="rounded-full bg-blue-500 w-2 h-2 mr-4"></div>
+                <div>C#</div>
+              </div>
+              <div className="text-center text-2xl pt-2 pb-2 text-neutral-700 flex items-center justify-start">
+                <div className="rounded-full bg-blue-500 w-2 h-2 mr-4"></div>
+                <div>JAVA</div>
               </div>
             </div>
           </div>
           <div className="bg-white w-full rounded-xl p-8 mt-8">
-            <div className="text-2xl font-semibold pb-2 border-b border-grey-500">
+            <div className="text-2xl font-semibold pb-2 border-b border-blue-600">
               Mobile App
             </div>
             <div className="mt-8">
-              <img src="/img/mobile-app.png" alt="mobile-app" />
+              <div className="text-center text-2xl pt-2 pb-2 text-neutral-700 flex items-center justify-start">
+                <div className="rounded-full bg-blue-500 w-2 h-2 mr-4"></div>
+                <div>React Native</div>
+              </div>
             </div>
           </div>
           <div className="bg-white w-full rounded-xl p-8 mt-8">
-            <div className="text-2xl font-semibold pb-2 border-b border-grey-500">
+            <div className="text-2xl font-semibold pb-2 border-b border-blue-600">
               Deployment
             </div>
             <div className="mt-8">
-              <img src="/img/deployment_01.png" alt="deployment_01" />
-              <img src="/img/backend_01.png" alt="deployment_02" />
+              <div className="text-center text-2xl pt-2 pb-2 text-neutral-700 flex items-center justify-start">
+                <div className="rounded-full bg-blue-500 w-2 h-2 mr-4"></div>
+                <div>netilify</div>
+              </div>
+              <div className="text-center text-2xl pt-2 pb-2 text-neutral-700 flex items-center justify-start">
+                <div className="rounded-full bg-blue-500 w-2 h-2 mr-4"></div>
+                <div>Vercel</div>
+              </div>
+              <div className="text-center text-2xl pt-2 pb-2 text-neutral-700 flex items-center justify-start">
+                <div className="rounded-full bg-blue-500 w-2 h-2 mr-4"></div>
+                <div>Firebase</div>
+              </div>
             </div>
           </div>
         </div>
-        <div className="md:w-4/12 w-full flex justify-center flex-col  mt-8 pl-6 pr-6 md:pl-6  md:mt-0">
-          <div className="bg-white w-full rounded-xl p-8  md:pl-6 pl-0 ">
-            <div className="text-2xl font-semibold pb-2 border-b border-grey-500">
+        <div className="md:max-xl:w-6/12 lg:w-4/12 w-full flex justify-center flex-col  mt-8 pl-6 pr-6 md:pl-6  md:mt-0">
+          <div className="bg-white w-full rounded-xl p-8  md:pl-6">
+            <div className="text-2xl font-semibold pb-2 border-b border-blue-600">
               Version Control
             </div>
             <div className="mt-8">
-              <img src="/img/version-control.png" alt="version-control" />
+              <div className="text-center text-2xl pt-2 pb-2 text-neutral-700 flex items-center justify-start">
+                <div className="rounded-full bg-blue-500 w-2 h-2 mr-4"></div>
+                <div>Git</div>
+              </div>
+              <div className="text-center text-2xl pt-2 pb-2 text-neutral-700 flex items-center justify-start">
+                <div className="rounded-full bg-blue-500 w-2 h-2 mr-4"></div>
+                <div>GitHub</div>
+              </div>
+              <div className="text-center text-2xl pt-2 pb-2 text-neutral-700 flex items-center justify-start">
+                <div className="rounded-full bg-blue-500 w-2 h-2 mr-4"></div>
+                <div>svn</div>
+              </div>
+              {/* <img src="/img/version-control.png" alt="version-control" /> */}
             </div>
           </div>
           <div className="bg-white w-full rounded-xl p-8 mt-8 ">
-            <div className="text-2xl font-semibold pb-2 border-b border-grey-500">
+            <div className="text-2xl font-semibold pb-2 border-b border-blue-500">
               Communication
             </div>
             <div className="mt-8">
-              <img src="/img/communication.png" alt="communication" />
+              <div className="text-center text-2xl pt-2 pb-2 text-neutral-700 flex items-center justify-start">
+                <div className="rounded-full bg-blue-500 w-2 h-2 mr-4"></div>
+                <div>Jira</div>
+              </div>
+              <div className="text-center text-2xl pt-2 pb-2 text-neutral-700 flex items-center justify-start">
+                <div className="rounded-full bg-blue-500 w-2 h-2 mr-4"></div>
+                <div>Confluence</div>
+              </div>
+              <div className="text-center text-2xl pt-2 pb-2 text-neutral-700 flex items-center justify-start">
+                <div className="rounded-full bg-blue-500 w-2 h-2 mr-4"></div>
+                <div>Zeplin</div>
+              </div>
+              <div className="text-center text-2xl pt-2 pb-2 text-neutral-700 flex items-center justify-start">
+                <div className="rounded-full bg-blue-500 w-2 h-2 mr-4"></div>
+                <div>Figma</div>
+              </div>
+              <div className="text-center text-2xl pt-2 pb-2 text-neutral-700 flex items-center justify-start">
+                <div className="rounded-full bg-blue-500 w-2 h-2 mr-4"></div>
+                <div>Slack</div>
+              </div>
             </div>
           </div>
         </div>
